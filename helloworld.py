755484560984d69faa648b888a4bc8c762bff39d
dcsohn <dcsohn@umich.edu>
2019-09-15 21:09:25 -0400

    Pushing some python code

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+# Author: David Sohn (dcsohn)
+# Class: EECS 388
+# Lab 0
+
+from random import randint
+
+a = randint(1, 101)
+b = randint(1, 101)
+
+sum = a + b
+
+print("David Sohn")
+print(a)
+print(b)
+print("Sum = " + str(sum))
+print("Average = " + str(sum / 2.0))
